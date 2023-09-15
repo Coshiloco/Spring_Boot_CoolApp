8f143aaee515e93c3e1c85a8b1a7f51a23db5a48
Coshiloco <pablohurtadohg@gmail.com>
2023-09-15 11:50:49 +0200

    Test Application Properties

--- src/main/java/com/pablohgdev/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/pablohgdev/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,11 +1,18 @@
 package com.pablohgdev.springboot.demo.mycoolapp.rest;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
 
     // expose "/" that return "Hello World"
 
@@ -14,5 +21,14 @@ public class FunRestController {
         return "Hello World!";
     }
 
+    @GetMapping("/coachName")
+    public String sayCoach() {
+        return coachName;
+    }
+
+    @GetMapping("/teamName")
+    public String sayTeamName() {
+        return teamName;
+    }
 
 }
